4b33572868f8b906255c6cc687521541f72c17fb
saswatsamal <samalsaswat0@gmail.com>
2021-04-24 09:53:38 +0530

    setting more colors

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ import random
 pygame.init()
 
 # Defining the colors
-white = (255,255,255)
-black = (0,0,0)
-red = (255,0,0)
-orange = (255,165,0)
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+blue = (50, 153, 213)
 
 width, height = 600, 400
 
