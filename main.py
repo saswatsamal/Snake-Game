525733e9e057a801b2706c6ca316809e2b6df032
saswatsamal <samalsaswat0@gmail.com>
2021-04-24 10:13:40 +0530

    yaay game is running

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ pygame.init()
  
 white = (255, 255, 255)
 yellow = (255, 255, 102)
-black = (0, 0, 0)
+black = (50, 153, 213)
 red = (213, 50, 80)
 green = (0, 255, 0)
-blue = (50, 153, 213)
+blue = (0,0,0)
  
 dis_width = 600
 dis_height = 400
  
 dis = pygame.display.set_mode((dis_width, dis_height))
-pygame.display.set_caption('Snake Game by Edureka')
+pygame.display.set_caption('Snake Game By Saswat Samal')
  
 clock = pygame.time.Clock()
  
