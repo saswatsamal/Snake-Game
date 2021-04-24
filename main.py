52034a93aec6ea074fe25f8da1c3d80eaa3d44b3
saswatsamal <samalsaswat0@gmail.com>
2021-04-24 08:30:18 +0530

    Setuping the Pygame - the overall display

--- main.py
+++ main.py
@@ -2,3 +2,18 @@ import pygame
 import time
 import random
 
+# Setuping the Pygame - the overall display
+
+#Init pygame
+pygame.init()
+
+# Defining the colors
+white = (255,255,255)
+black = (0,0,0)
+red = (255,0,0)
+orange = (255,165,0)
+
+width, height = 600, 400
+
+gameDisplay = pygame.display.set_mode((width,height))
+pygame.display.set_caption("Snake Game")
