9b85e0daebd014f5e1fa5a8488bce898e993e2c1
saswatsamal <samalsaswat0@gmail.com>
2021-04-24 08:39:12 +0530

    Defining the functions

--- main.py
+++ main.py
@@ -28,3 +28,13 @@ snakeSpeed = 15
 # Setting the font
 messageFont = pygame.font.SysFont('ubuntu', 30)
 scoreFont = pygame.font.SysFont('ubuntu', 25)
+
+# Defining the functions - updating/drawing the score and the snake wrt the position
+def printScore(score):
+    text = scoreFont.render("Score: "+str(score), True, orange)
+    gameDisplay.blit(text, [0,0])
+
+def drawSnake(snakeSize,snakePixels):
+    for pixel in snakePixels:
+        pygame.draw.rect(gameDisplay, white, [pixel[0], pixel[1], snakeSize, snakeSize])
+
